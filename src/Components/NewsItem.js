6eb904e5a7e139e6c6e305dc742a80ad66b79bb6
Mahir Shahriar Tamim <03mahirshahriar@gmail.com>
2023-02-18 00:24:48 +0600

    new branch

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -15,7 +15,7 @@ export class NewsItem extends Component {
         <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: 1}}>
             {source}   
         </span>
-          <img src={imageUrl?imageUrl:"https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg  "} className="card-img-top" alt="..." />
+          <img  src={imageUrl?imageUrl:"https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg  "} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title.length>45?title.slice(0,45)+"...":title}</h5>
             {/* {console.log(title.length)} */}
