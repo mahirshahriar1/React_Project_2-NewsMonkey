d274c4f2256dd2255cf32f4ae7a1441423757f8b
Mahir Shahriar Tamim <03mahirshahriar@gmail.com>
2023-02-18 00:26:39 +0600

    test

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -14,7 +14,7 @@ export class NewsItem extends Component {
         <div className="card" >
         <span className="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: 1}}>
             {source}   
-        </span>
+        </span >
           <img src={imageUrl?imageUrl:"https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg  "} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title.length>45?title.slice(0,45)+"...":title}</h5>
